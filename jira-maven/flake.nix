8c4cb575670114840075007d2376048808cb1d87
Patrick Louis <patrick.louis@linked-planet.com>
2025-05-28 08:14:50 +0200

    CHG Remove unusable flake outputs

--- jira-maven/flake.nix
+++ jira-maven/flake.nix
@@ -13,9 +13,6 @@
     , ...
     }:
     let
-      packageName = throw "Please provide a name for this package";
-      packageVersion = throw "Pleases provide the version of this package";
-
       javaVersion = 11;
       nodeVersion = 20;
       postgresVersion = 14;
@@ -23,12 +20,12 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: let 
-          pkgs = nixpkgs.legacyPackages.${system} // { overlays = [ self.overlays.default ]; };
-        in f pkgs);
+        pkgs = nixpkgs.legacyPackages.${system} // { overlays = [ self.overlays.default ]; };
+      in f pkgs);
 
       treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
     in
-    rec {
+      rec {
       overlays.default = (final: prev: rec {
         jdk = prev."jdk${toString javaVersion}";
         maven = prev.maven.override { jdk_headless = jdk; };
@@ -56,56 +53,12 @@
         };
       });
 
-      packages = eachSystem (pkgs: {
-        default = (pkgs.maven.buildMavenPackage rec {
-          pname = packageName;
-          version = packageVersion;
-
-          mvnHash = "sha256-HDfPn6Vdhrgcp9rDJYTiw0AvnlmZ6QCIZrJ1C779Xcg=";
-
-          src = builtins.path { path = ./.; };
-          buildInputs = [
-            pkgs.maven
-            pkgs.jdk
-            pkgs.nodejs
-          ];
-
-          installPhase = ''
-            mkdir -p $out/
-            cp "target" $out/share
-          '';
-        });
-      });
-
-      apps = eachSystem (pkgs: {
-        pkgs.system.overlays = self.overlays.default;
-        deploy = {
-          program = pkgs.lib.getExe (pkgs.writeShellScriptBin "upmDeploy" ''
-            ${pkgs.maven}/bin/mvn \
-              upm:uploadPluginFile \
-              -Ddeploy.url=http://localhost:8080 \
-              -Ddeploy.username=admin \
-              -Ddeploy.password=admin \
-              -Dfrontend.devMode=true \
-              -Dplugin.file=${packages.${pkgs.system}.default}/share/${packageName}-${packageVersion}.jar \
-              -Plocal
-          '');
-          type = "app";
-        };
-      });
-
       formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-      # for `nix flake check`
       checks = eachSystem (pkgs: {
         formatting = treefmtEval.${pkgs.system}.config.build.check self;
       });
 
-    defaultTemplate = {
-      path = ".";
-      description = "Flake for use with atlassian-maven projects";
-    };
-
-    # Utilized by `nix flake init -t <flake>#<name>`
-    templates.example = self.defaultTemplate;
+      # Utilized by `nix flake init -t <flake>#<name>`
+      templates.example = self.defaultTemplate;
     };
 }
